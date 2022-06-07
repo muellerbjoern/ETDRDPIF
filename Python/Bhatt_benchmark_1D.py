fe3141a648905870904edc61051dfe529f0cf17f
Björn Müller <bj.mueller@fz-juelich.de>
2022-06-07 17:09:31 -0500

    Initial translation to Python
    Contains only periodic boundary and "standard" solve, may still contain
    errors

---
+++ Python/Bhatt_benchmark_1D.py
@@ -0,0 +1,78 @@
+import numpy as np
+import scipy.sparse as sp
+
+from etdrdpif.solve import etd_solve
+from etdrdpif.discretize_periodic import discretize_periodic
+
+
+# Benchmark problem from Bhatt et al., 2018, reduced to single dimension,
+# but still 2 species!
+
+def Bhatt_benchmark_1D(dt, steps):
+    # dt: time step. Default is 0.001
+    # steps: number of spatial points in each coordinate direction. Default is 11
+
+    # k is temporal discretization (dt); here: 0.005
+    # h is spatial discretization (steps); here: 0.1
+
+    dim = 1
+    num_species = 2
+
+    te = 1.0
+    square_len = 2*np.pi
+
+
+    ## Model Paramters and initial conditions
+    a = 3.0
+    #d = 0.1
+    d=1.0
+    Adv = a/1.0*np.ones((num_species, dim))
+    Diff = d/1.0*np.ones((num_species, dim))
+
+    #b = 0.1;
+    b = 100.0
+    c = 1.0
+
+    # Discretize time interval
+    t = np.arange(0, te+dt, dt)
+    tlen = len(t)
+
+    # Discretize in space
+    x, steps, nodes, A = discretize_periodic(steps, square_len, Diff, Adv)
+
+    # Both species treated separately!
+    # Possible due to assumption of no coupling in diffusive term
+    # initial condition for u
+    u_old = 2*np.cos(np.sum(nodes, axis=1))
+    # initial condition for v
+    v_old = (b-c)*np.cos(np.sum(nodes, axis=1))
+    u_old = [u_old, v_old]
+
+    def F(u):
+        f0 = -b * u[0] + u[1]
+        f1 = -c * u[1]
+        Fr = [f0, f1]
+        return Fr
+
+    runtime, soln = etd_solve(dt, tlen, steps, A, u_old, F)
+
+    u_soln = soln[0];
+    v_soln = soln[1];
+
+    Uex = (np.exp(-b-d)+np.exp(-c-d))*np.cos(np.sum(nodes, axis=1)-a);
+    Vex = (b-c)*np.exp(-c-d)*np.cos(np.sum(nodes, axis=1)-a);
+
+    Uex = np.reshape(Uex, (steps, 1))
+    u_ex = Uex
+    Vex = np.reshape(Vex, (steps, 1))
+    Usoln = np.reshape(u_soln, (steps, 1))
+    Vsoln = np.reshape(v_soln, (steps, 1))
+
+    print(max(max(Usoln - Uex)));
+
+    #if do_plot:
+    #    plot_soln(Usoln, Vsoln, Uex, Vex, {x, x})
+
+
+if __name__ == "__main__":
+    Bhatt_benchmark_1D(0.00001, 100000)
---
+++ Python/etdrdpif/discretize_periodic.py
@@ -0,0 +1,97 @@
+import numpy as np
+import scipy.sparse as sp
+
+
+def discretize_periodic(steps, square_len, Diff, Adv):
+
+    dim = Adv.shape[1]
+
+    # create nodes
+    # Split interval into steps subintervals (i.e., steps+1 points, including the
+    # end of the interval
+    x = np.linspace(0,square_len,steps+1)
+    h = abs(x[0]-x[1])
+
+    # Remove the very last point, i.e., the end of the interval, as it is the same
+    # as the very first (periodic boundary!)
+    x = x[:-1]
+
+    # Create nodes using ndgrid
+    # This is limited to a (hyper)square domain currently
+    # For other (hyper)rectangles, explicitly specify grid vectors for all
+    # dimensions
+    nodes = np.array(np.meshgrid(*([x]*dim), indexing='ij'))  # TODO: Check if this is really desired
+    nodes = nodes.reshape(dim, -1).T
+    nodes = nodes [:, ::-1]  # TODO: In Matlab this is the result, but do I want that in Python?
+
+    # Block matrix Assembly
+    # Matrix for the 1D, 1 species case
+    e = np.ones(steps)
+    r = 1/h**2
+    B = sp.spdiags([-r*e, 2*r*e, -r*e], [-1, 0, 1], steps, steps, format='csc')
+
+    # Periodic boundary condition
+    B[0, -1] = -r
+    B[-1, 0] = -r
+
+    # Advection matrix analogously
+    r_adv = 1 / (2 * h)
+    C = sp.spdiags([-r_adv * e, r_adv * e], [-1,1], steps, steps, format='csc')
+    C[0, -1] = -r_adv
+    C[-1, 0] = r_adv
+
+    A = adapt_dimension(B, C, Diff, Adv)
+
+    return x, steps, nodes, A
+
+
+def adapt_dimension(B, C, Diff, Adv):
+    # Diff and Adv must be (num_species x dim) matrices.
+    # This enables setting diffusion and advection per species and
+    # per dimension.
+    # Dependence on spatial dimension is mostly required for advection as
+    # advection is usually defined along a certain spatial vector
+    # Different diffusion constants per species necessitate the ability
+    # of setting constants per species
+    # In order to keep it general, we allow both dependencies
+
+    num_species, dim = Diff.shape
+    if Diff.shape != Adv.shape:
+        msg = 'Advection and Diffusion constant matrices need to be '\
+            '(number of species x spatial dimension)'
+        raise ValueError(msg)
+
+    steps = B.shape[0]
+    if B.shape != C.shape:
+        msg = 'Advection and Diffusion matrices (spatial discretization)'\
+            ' need to be (steps x steps)'
+        raise ValueError(msg)
+
+    A = []  # dimensions x species  # TODO: NOTE! Different from Matlab
+
+    I = sp.eye(steps, format='csc')
+
+    for i_dim in range(1, dim+1):
+        I_left = 1
+        for ii in range(i_dim+1, dim+1):
+            I_left = sp.kron(I_left, I)
+
+        I_right = 1
+        for ii in range(2, i_dim+1):
+           I_right = sp.kron(I_right, I)
+
+        A.append([])
+        for i_spec in range(num_species):
+            A[i_dim-1].append(Diff[i_spec,i_dim-1]*sp.kron(I_left, sp.kron(B, I_right)) +  Adv[i_spec,i_dim-1]*sp.kron(I_left, sp.kron(C, I_right)))
+
+    return A
+
+    # 3D equivalent:
+    # Ax = Diff(1,1)*kron(I,kron(I,B)) + Adv(1,1)*kron(I,kron(I,C));
+    # Ay = Diff(1,1)*kron(I,kron(B,I)) + Adv(1,1)*kron(I,kron(C,I));
+    # Az = Diff(1,1)*kron(B,kron(I,I)) + Adv(1,1)*kron(C,kron(I,I));
+
+
+if __name__ == "__main__":
+    A = discretize_periodic(10, 1, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]), np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+    print(A)
