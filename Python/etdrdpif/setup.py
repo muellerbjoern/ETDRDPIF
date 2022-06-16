5fd504c215b1ab1a91390ef586982e3013f7d259
Björn Müller <bj.mueller@fz-juelich.de>
2022-06-16 12:01:06 -0500

    Correct setup.py

---
+++ Python/etdrdpif/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ETD-RDP-IF',
+      version='0.1',
+      description='ETD solver for Advection-Diffusion-Reaction Equations',
+      author='Andreas Kleefeld',
+      packages=['etdrdpif'],
+     )
