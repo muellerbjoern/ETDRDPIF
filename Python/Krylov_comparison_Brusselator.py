72680cf235dc58f198ade88bc0ec0e9c52258e17
Björn Müller <bj.mueller@fz-juelich.de>
2022-07-15 16:19:29 -0500

    Added more stuff for Krylov Brusselator comparison

--- Python/Krylov_comparison_Brusselator.py
+++ Python/Krylov_comparison_Brusselator.py
@@ -3,6 +3,8 @@ import numpy as np
 import warnings
 import sys
 
+import scipy.io
+
 from Krylov_solve import Krylov_solve
 from solver.etdrdpif.wrapper import wrap_Krylov, wrap_solve
 
@@ -18,76 +20,76 @@ def main(solver, discretization=None):
             discretization = 'central'
 
     # Parameters of the specific experiment
-    square_len = 2 * np.pi
+    square_len = 1.0
     te = 1.0
-    d = 1.0
-    Diff = [d / 3, d / 3]
-    a = 3.0
-    Adv = [a/3, a/3]
+    Diff = [0.02, 0.01]
+    A = 1.0
 
-    b = 100.0
-    c = 1.0
+    b = 2.0
 
-    boundary = 'periodic'
+    boundary = 'Neumann'
 
     def u0(x, y, z):
-        return 2 * np.cos(x + y + z), (b - c) * np.cos(x + y + z)
+        return 1.0 + np.sin(2 * np.pi * x) * np.sin(2 * np.pi * y) * np.sin(2 * np.pi * z), 3.0#*np.ones_like(x)
 
     def F(U1, V1):
-        return -b * U1 + V1, -c * V1
-
-    # runtime_Krylov, U_sol_Krylov = Krylov_solve(te, dt, steps, square_len, Adv, Diff, F, u0)
-
-    runtime_rdp, U_sol_rdp = wrap_Krylov(te, 0.005, 10, square_len, Adv, Diff, F, u0)
-
-    # print(np.max(U_sol_Krylov.flatten() - U_sol_rdp.flatten()))
-
-    x = np.linspace(0, square_len, 10 + 1)[:-1]
-    nodes = np.array(np.meshgrid([x], [x], [x], indexing='ij'))
-    Uex = (np.exp(-b - d) + np.exp(-c - d)) * np.cos(np.sum(nodes, axis=0) - a).flatten()
-
-    # print(np.max(U_sol_Krylov-Uex))
-    print(np.max(U_sol_rdp-Uex))
+        return U1**2 * V1 - (A + 1) * U1 + b, A*U1 - U1**2 * V1
 
     a_vals = [0.01, 0.1, 0.15, 0.2, 0.25, 0.3, 0.5, 1, 2, 3, 5, 10, 20, 100, 200, 500, 1000]
+    a_vals = [1.0]
     n = 5
     errors_euclid = np.zeros((len(a_vals), n))
     errors_max = np.zeros((len(a_vals), n))
     orders_euclid = np.zeros((len(a_vals), n - 1))
     orders_max = np.zeros((len(a_vals), n - 1))
-    runtimes = np.zeros((len(a_vals), n))
-    params = np.zeros((len(a_vals), n, 3))
+    runtimes = np.zeros((len(a_vals), n+1))
+    params = np.zeros((len(a_vals), n+1, 3))
     for i, a in enumerate(a_vals[::-1]):
+        Adv = [a, a]
+        steps = 32
+        k = 0.005
+        runtime, sol = solve(te, k, steps, square_len, Adv, Diff, F, u0, boundary=boundary,
+                             discretization=discretization)
+        print(np.min(sol), np.max(sol))
+        matsoln = scipy.io.loadmat("../MATLAB/matsoln.mat")["usoln"]
+        matsoln = matsoln.flatten()
+        diff = sol - matsoln
+        diff_max = np.max(np.abs(diff))
+        runtimes[i, 0] = runtime
+        params[i, 0] = np.array([a, 1.0/steps, k])
         err_old = np.inf
         err_max_old = np.inf
-        Adv = [a/3.0, a/3.0]
-        for j, h_exp in enumerate(range(n)):
-            steps = 10*2**h_exp
-            k = 0.005/(2**h_exp)
-            x = np.linspace(0, square_len, steps + 1)[:-1]
-            nodes = np.array(np.meshgrid([x], [x], [x], indexing='ij'))
-            Uex = (np.exp(-b - d) + np.exp(-c - d)) * np.cos(np.sum(nodes, axis=0) - a).flatten()
+        for j, h_exp in enumerate(range(1, n)):
+            #steps = 10*2**h_exp
+            k = k/2
+            #k = 0.005/(2**h_exp)
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
                 try:
-                    runtime, sol = solve(te, k, steps, square_len, Adv, Diff, F, u0, boundary,
+                    print(k, steps)
+                    runtime, sol_new = solve(te, k, steps, square_len, Adv, Diff, F, u0, boundary,
                                          discretization=discretization)
+                    runtime_etd, sol_etd = wrap_Krylov(te, k, steps, square_len, Adv, Diff, F, u0, boundary,
+                                         discretization=discretization)
+                    print("Difference", np.max(np.abs(sol_etd-sol_new)))
+                    print(np.min(sol_new), np.max(sol_new))
                 except Exception as e:
                     print("Error occurred")
                     print(e)
                     continue
-            err = (Uex - sol).flatten()
+            err = (sol_new - sol).flatten()
+            sol = sol_new
             err_euclid = np.linalg.norm(err)
-            err_max = np.linalg.norm(err, np.inf)
+            err_max = np.max(np.abs(err))
             order = np.log2(err_old / err_euclid)
-            h = 2*np.pi/steps
-            print(f"a={a}, C={a*k/h}, h={h}, k={k},\t error={err_max}, order={order}")
+            h = 1.0/steps
+            print(f"a={a}, C={a*k/h}, h={h}, k={2*k},\t error={err_max}, order={order}")
             errors_euclid[i, j] = err_euclid
             errors_max[i, j] = err_max
-            runtimes[i, j] = runtime
-            params[i, j, 0] = a
-            params[i, j, 1] = h
-            params[i, j, 2] = k
+            runtimes[i, j+1] = runtime
+            params[i, j+1, 0] = a
+            params[i, j+1, 1] = h
+            params[i, j+1, 2] = k
             if j > 0:
                 orders_euclid[i, j-1] = order
                 orders_max[i, j-1] = np.log2(err_max_old/err_max)
@@ -95,7 +97,7 @@ def main(solver, discretization=None):
             err_max_old = err_max
             sys.stdout.flush()
     sys.stdout.flush()
-    experiment = f"Bhatt_ex1_{solver}_{discretization}"
+    experiment = f"Bhatt_Brusselator_{solver}_{discretization}"
     np.save(f"{experiment}_errors_euclid", errors_euclid)
     np.save(f"{experiment}_errors_max.npy", errors_max)
     np.save(f"{experiment}_orders_euclid.npy", orders_euclid)
